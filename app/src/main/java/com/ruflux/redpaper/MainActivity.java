b85e8fee1fe1db6a273a460791380d791e7274c7
Rupak <rupak0577@gmail.com>
2018-06-05 00:14:39 +0530

    Remove old code

--- app/src/main/java/com/ruflux/redpaper/MainActivity.java
+++ app/src/main/java/com/ruflux/redpaper/MainActivity.java
@@ -2,13 +2,9 @@ package com.ruflux.redpaper;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 
 import com.ruflux.redpaper.databinding.ActivityMainBinding;
-import com.ruflux.redpaper.sub.SubFragment;
-import com.ruflux.redpaper.sub.SubPresenter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,44 +16,5 @@ public class MainActivity extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         setSupportActionBar(binding.toolbarPrimary);
-
-        final SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        binding.viewpagerPrimary.setAdapter(adapter);
-        binding.tablayoutPrimary.setupWithViewPager(binding.viewpagerPrimary);
-    }
-
-    private class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        private SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public SubFragment getItem(int position) {
-            SubFragment fragment = SubFragment.newInstance(0);
-            new SubPresenter(fragment);
-            return fragment;
-        }
-
-        @Override
-        public int getCount() {
-            return 4;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "EARTH";
-                case 1:
-                    return "ROAD";
-                case 2:
-                    return "RURAL";
-                case 3:
-                    return "ABANDONED";
-            }
-            return null;
-        }
     }
 }
